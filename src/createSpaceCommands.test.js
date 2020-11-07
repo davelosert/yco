e9e51247b1d56f8db08618d684c8f13285609d57
David Losert <david.losert@gmx.de>
2020-11-07 17:02:21 +0100

    chore: improved test descriptions

--- src/createSpaceCommands.test.js
+++ src/createSpaceCommands.test.js
@@ -1,5 +1,5 @@
 import { describe } from 'riteway';
-import { createSpaceCommands } from './createSpaceCommands'
+import { createSpaceCommands } from './createSpaceCommands';
 
 describe('createSpaceCommands()', async assert => {
   const yabaiCreateSpace = 'yabai -m space --create';
@@ -34,17 +34,17 @@ describe('createSpaceCommands()', async assert => {
 
   assert({
     given: 'two spaces to create on second display',
-    should: 'return a space create and move command with the index of the first display',
+    should: 'return a space create and move command with the space index higher than the maximum of the first display',
     actual: createSpaceCommands({ spacesPlan: [0, 2], spacesCount: [2, 1] }),
     expected: [yabaiCreateSpace, 'yabai -m space 3 --display 2', yabaiCreateSpace, 'yabai -m space 3 --display 2']
   });
 
   assert({
     given: 'one space to create on first and second display',
-    should: 'return the move command with the calculated index',
+    should: 'return the move command with the anticipated indices the second display',
     actual: createSpaceCommands({ spacesPlan: [1, 1], spacesCount: [2, 2] }),
     expected: [yabaiCreateSpace, yabaiCreateSpace, 'yabai -m space 4 --display 2']
-  })
+  });
 
   assert({
     given: 'one space to delete on first display',
@@ -55,14 +55,14 @@ describe('createSpaceCommands()', async assert => {
 
   assert({
     given: 'two spaces to delete on first display',
-    should: 'return only two destroy commands',
+    should: 'return two destroy commands',
     actual: createSpaceCommands({ spacesPlan: [-2], spacesCount: [3] }),
     expected: ['yabai -m space 3 --destroy', 'yabai -m space 2 --destroy']
   });
 
   assert({
     given: 'one space to create on first and two to delete on second display',
-    should: 'return destroy commands with calculated max index',
+    should: 'return destroy commands with anticipated index',
     actual: createSpaceCommands({ spacesPlan: [1, -2], spacesCount: [2, 3] }),
     expected: [yabaiCreateSpace, 'yabai -m space 6 --destroy', 'yabai -m space 5 --destroy']
   });
