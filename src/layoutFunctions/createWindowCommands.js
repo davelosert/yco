258bff55e2c82a004dc3f3bbc096f341629e9e19
David Losert <david.losert@gmx.de>
2020-11-14 17:06:02 +0100

    chore: Improved createWindowCommands readability

--- src/layoutFunctions/createWindowCommands.js
+++ src/layoutFunctions/createWindowCommands.js
@@ -1,16 +1,16 @@
-const createMoveWindowCommand = (windowId, targetSpaceIndex) => `yabai -m window ${windowId} --space ${targetSpaceIndex}`;
-export const createWindowCommands = (desiredWindowLayout) => {
-  const flattenedSpacesLayout = desiredWindowLayout.reduce((accum, spacesSetup) => [
-    ...accum,
-    ...spacesSetup
-  ], []);
+import { addIndex, pipe, reduce, unnest } from 'ramda';
 
-  return flattenedSpacesLayout.reduce((commands, windows, targetSpaceIndex) => {
-    windows.forEach(window => {
-      if(window.space !== targetSpaceIndex + 1) {
-        commands.push(createMoveWindowCommand(window.id, targetSpaceIndex + 1));
-      }
-    });
-    return commands;
-  }, []);
-};
+const reduceIndexed = addIndex(reduce);
+const createMoveWindowCommand = (windowId, targetSpaceIndex) => `yabai -m window ${windowId} --space ${targetSpaceIndex}`;
+
+export const createWindowCommands = (desiredWindowLayout) => pipe(
+  unnest,
+  reduceIndexed((commands, windows, targetSpaceIndex) => {
+      windows.forEach(window => {
+        if(window.space !== targetSpaceIndex + 1) {
+          commands.push(createMoveWindowCommand(window.id, targetSpaceIndex + 1));
+        }
+      });
+      return commands;
+    }, [])
+)(desiredWindowLayout);
