aedb5e83e1418931781f6752775a0b5bdb1a69b9
David Losert <david.losert@gmx.de>
2020-12-21 14:49:20 +0100

    chore: Added semantic-release to project

---
+++ release.config.js
@@ -0,0 +1,10 @@
+const config = {
+  'plugins': [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/npm',
+    '@semantic-release/git'
+  ]
+};
+
+module.exports = config;
