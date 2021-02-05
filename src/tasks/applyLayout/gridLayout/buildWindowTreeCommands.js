1d3a6c1776da12ca7e743612468f393c4081c22d
David Losert <david.losert@gmx.de>
2021-02-05 13:52:49 +0100

    chore: minor code cleanups

--- src/tasks/applyLayout/gridLayout/buildWindowTreeCommands.js
+++ src/tasks/applyLayout/gridLayout/buildWindowTreeCommands.js
@@ -1,45 +1,56 @@
-const { flatten } = require('ramda');
+const { generateInsertCommand, generateWarpCommand } = require('../../../shared/yabaiCommands');
 
-const buildWindowTreeCommands = (rootNode) => {
-  const [firstNode, ...siblings] = rootNode.windows;
-  const firstWindow = getMostLeftWindow(firstNode);
-  const childNodes = [];
-  if (firstNode.type === 'treeNode') {
-    childNodes.push(firstNode);
-  }
-
-  const direction = rootNode.split === 'vertical' ? 'east' : 'south';
-  const siblingContext = siblings.reduce((context, currentNode) => {
-    const currentWindow = getMostLeftWindow(currentNode);
-
-    if (currentNode.type === 'treeNode') {
-      childNodes.push(currentNode);
-    }
-
-    return {
-      commands: [
-        ...context.commands,
-        `yabai -m window ${context.previousNode.id} --insert ${direction}`,
-        `yabai -m window ${currentWindow.id} --warp ${context.previousNode.id}`
-      ],
-      previousNode: currentWindow
-    };
-
-  }, { commands: [], previousNode: firstWindow });
-  let commands = siblingContext.commands;
-
+const buildWindowTreeCommands = (parentNode) => {
+  let { commands, childNodes } = handleSiblings(parentNode);
 
   if (childNodes.length > 0) {
-    const subCommands = flatten(childNodes.map(subTree => buildWindowTreeCommands(subTree)));
+    const childCommands = childNodes.flatMap(subTree => buildWindowTreeCommands(subTree));
     commands = [
       ...commands,
-      ...subCommands
+      ...childCommands
     ];
   }
 
   return commands;
 };
 
+function handleSiblings(parentNode) {
+  const childNodes = [];
+  const insertDirection = parentNode.split === 'vertical' ? 'east' : 'south';
+
+  const { commands } = parentNode.windows.reduce((context, currentNode) => {
+    if (currentNode.type === 'treeNode') {
+      childNodes.push(currentNode);
+    }
+
+    const sourceWindow = getMostLeftWindow(currentNode);
+    if (isFirstIteration(context)) {
+      return {
+        commands: [],
+        previousWindow: sourceWindow
+      };
+    }
+
+
+    return {
+      commands: [
+        ...context.commands,
+        generateInsertCommand(context.previousWindow.id, insertDirection),
+        generateWarpCommand(sourceWindow.id, context.previousWindow.id)
+      ],
+      previousWindow: sourceWindow
+    };
+
+  }, { commands: [], previousWindow: null });
+  return {
+    commands,
+    childNodes
+  };
+}
+
+function isFirstIteration(context) {
+  return !context.previousWindow;
+}
 
 function getMostLeftWindow(node) {
   if (node.type === 'window') {
